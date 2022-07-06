50400095af6f77ac4f65950864d152dd69f4fd8a
dodaniritika <108716342+dodaniritika@users.noreply.github.com>
2022-07-06 12:47:06 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
+
 public class Main
 {
   public static double add (double a, double b)
   {
     return a + b;
-}
+  }
   public static double divide (double a, double b)
   {
-        if(b == 0) {  
-            throw new ArithmeticException("Division by 0");    
-        }  
-        else {  
-            return a/b;
-        }
-}
+    if (b == 0)
+      {
+	throw new ArithmeticException ("Division by 0");
+      }
+    else
+      {
+	return a / b;
+      }
+  }
   public static double subtract (double a, double b)
   {
     return a - b;
@@ -24,10 +27,10 @@ public class Main
     double a = 2.5;
     double b = 5.865;
 
-    System.out.println(add (a, b));
+    System.out.println (add (a, b));
 
-    System.out.println(divide (a, b));
+    System.out.println (divide (a, b));
 
-    System.out.println(subtract (a, b));
+    System.out.println (subtract (a, b));
   }
 }
