e9b6f3b5e283d09da242679b6415492dcd14dd7f
dodaniritika <ritika.dodani@astera.com>
2022-07-06 12:32:00 +0500

    subtract function in main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main
 {
+  public static double subtract (double a, double b)
+  {
+    return a - b;
+  }
+
   public static void main (String[]args)
   {
+    double a = 5.865;
+    double b = 2.34;
+
+    System.out.println(subtract (a, b));
   }
 }
