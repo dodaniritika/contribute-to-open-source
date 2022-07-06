51b1d4d6ac02795ff0e410cf13717d57105c0a29
dodaniritika <ritika.dodani@astera.com>
2022-07-06 12:30:36 +0500

    add function in main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main
 {
+  public static double add (double a, double b)
+  {
+    return a + b;
+  }
+
   public static void main (String[]args)
   {
+    double a = 2.5;
+    double b = 5.865;
+
+    System.out.println(add (a, b));
   }
 }
