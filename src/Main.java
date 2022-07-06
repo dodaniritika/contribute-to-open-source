412c4376c8147d2c4048bb211ce112977eeeb5fe
dodaniritika <ritika.dodani@astera.com>
2022-07-06 12:38:18 +0500

    divide function in main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main
 {
+  public static double divide (double a, double b)
+  {
+        if(b == 0) {  
+            throw new ArithmeticException("Division by 0");    
+        }  
+        else {  
+            return a/b;
+        }
+  }
+
   public static void main (String[]args)
   {
+    double a = 5.865;
+    double b = 0.00001;
+
+    System.out.println(divide (a, b));
   }
 }
